282a94b0a68fd99cbd918907c7a8c77dcc656331
André Ferraz <15928408+andreferraz@users.noreply.github.com>
2026-05-12 21:34:15 -0300

    feat: add Radix UI framework

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
+import { Theme } from "@radix-ui/themes";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import "@radix-ui/themes/styles.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,7 +26,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body>{children}</body>
+      <body>
+        <Theme appearance="dark">{children}</Theme>
+      </body>
     </html>
   );
 }
